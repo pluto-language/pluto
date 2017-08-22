07f8fd7eacc29eef4d168ce2fa689e6f7a9f1b22
Zac Garby <zacgarby@icloud.com>
2017-08-22 17:20:56 +0100

    Define basic Object interfaces

---
+++ object/object.go
@@ -0,0 +1,39 @@
+package object
+
+import "fmt"
+
+type Type string
+
+const (
+	/* Internal Types */
+	RETURN_VALUE Type = "<return value>"
+	FUNCTION          = "<function>"
+	NEXT              = "<next>"
+	BREAK             = "<break>"
+
+	/* Normal Types */
+	NUMBER   Type = "<number>"
+	BOOLEAN       = "<boolean>"
+	STRING        = "<string>"
+	CHAR          = "<char>"
+	ARRAY         = "<array>"
+	NULL          = "<null>"
+	BLOCK         = "<block>"
+	TUPLE         = "<tuple>"
+	MAP           = "<map>"
+	CLASS         = "<class>"
+	INIT          = "<init method>"
+	METHOD        = "<method>"
+	INSTANCE      = "<instance>"
+)
+
+type Object interface {
+	fmt.Stringer
+	Equals(Object) bool
+	Type() Type
+}
+
+type Collection interface {
+	Object
+	Elements() []Object
+}
