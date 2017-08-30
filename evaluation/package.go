2053a418416164110fe07484ec180d558910ec7e
Zac Garby <zacgarby@icloud.com>
2017-08-30 19:18:23 +0100

    Define Package
    Represents a package which has been imported into another

---
+++ evaluation/package.go
@@ -0,0 +1,20 @@
+package evaluation
+
+import "github.com/Zac-Garby/pluto/ast"
+
+// Package represents an imported package
+type Package struct {
+	Context Context // the context containing the package's functions
+	Used    bool    // whether 'use <this package>' has been called
+
+	/* metadata read from 'collections.yaml' */
+	Meta struct {
+		Title, Description, Version string
+		Tags, Authors, Sources      []string
+	}
+}
+
+// GetFunction directly calls the context's GetFunction()
+func (p *Package) GetFunction(ptn []ast.Expression) interface{} {
+	return p.Context.GetFunction(ptn)
+}
