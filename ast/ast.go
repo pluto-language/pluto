83d49743edecf4f81665f376c1d97ad414955268
Zac Garby <zacgarby@icloud.com>
2017-08-31 16:57:52 +0100

    Add comments to AST

--- ast/ast.go
+++ ast/ast.go
@@ -4,24 +4,31 @@ import (
 	"github.com/Zac-Garby/pluto/token"
 )
 
+// Node is the base AST node struct
 type Node interface {
 	Token() token.Token
 }
 
+// Statement is a statement AST node
 type Statement interface {
 	Node
 	Stmt()
 }
 
+// Expression is an expression AST node
 type Expression interface {
 	Node
 	Expr()
 }
 
+// Program is a program, containing
+// a list of statements
 type Program struct {
 	Statements []Statement
 }
 
+// Tree returns a tree representation of
+// a program.
 func (p *Program) Tree() string {
 	str := ""
 
