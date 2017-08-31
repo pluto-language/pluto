8c07030243a7b6d34347ddd36af0b54b70c65514
Zac Garby <zacgarby@icloud.com>
2017-08-31 18:17:11 +0100

    Add comments to the object interfaces

--- evaluation/object.go
+++ evaluation/object.go
@@ -2,12 +2,17 @@ package evaluation
 
 import "fmt"
 
+// Object is the base object interface, which
+// every object implements
 type Object interface {
 	fmt.Stringer
 	Equals(Object) bool
 	Type() Type
 }
 
+// Collection is a child interface of Object,
+// which represents an object which can be
+// thought of as a list of items
 type Collection interface {
 	Object
 	Elements() []Object
@@ -15,12 +20,16 @@ type Collection interface {
 	SetIndex(int, Object)
 }
 
+// Container is a child interface of Object,
+// which can be accessed by keys - like a map
 type Container interface {
 	Object
 	Get(Object) Object
 	Set(Object, Object)
 }
 
+// Hasher is any object which can be a key
+// in a map
 type Hasher interface {
 	Object
 	Hash() string
