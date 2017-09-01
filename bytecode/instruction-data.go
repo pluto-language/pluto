a39ee6d84f10a9791630d45c3df91d428319ecbc
Zac Garby <zacgarby@icloud.com>
2017-09-01 17:53:37 +0100

    Add a table mapping instructions to their data

---
+++ bytecode/instruction-data.go
@@ -0,0 +1,34 @@
+package bytecode
+
+type data struct {
+	Name   string
+	HasArg bool
+}
+
+// Instructions stores data about the different instruction types
+var Instructions = map[int]data{
+	Pop: {Name: "POP"},
+	Dup: {Name: "DUP"},
+	Rot: {Name: "ROT"},
+
+	LoadConst: {"LOAD_CONST", true},
+	LoadName:  {"LOAD_NAME", true},
+	StoreName: {"STORE_NAME", true},
+
+	UnaryInvert: {Name: "UNARY_INVERT"},
+	UnaryNegate: {Name: "UNARY_NEGATE"},
+	UnaryNoOp:   {Name: "UNARY_NO_OP"},
+
+	BinaryAdd:      {Name: "BINARY_ADD"},
+	BinarySubtract: {Name: "BINARY_SUBTRACT"},
+	BinaryMultiply: {Name: "BINARY_MULTIPLY"},
+	BinaryDivide:   {Name: "BINARY_DIVIDE"},
+	BinaryExponent: {Name: "BINARY_EXPONENT"},
+	BinaryFloorDiv: {Name: "BINARY_FLOOR_DIV"},
+	BinaryMod:      {Name: "BINARY_MOD"},
+	BinaryOr:       {Name: "BINARY_OR"},
+	BinaryAnd:      {Name: "BINARY_AND"},
+	BinaryBitOr:    {Name: "BINARY_BIT_OR"},
+	BinaryBitAnd:   {Name: "BINARY_BIT_AND"},
+	BinaryEquals:   {Name: "BINARY_EQUALS"},
+}
