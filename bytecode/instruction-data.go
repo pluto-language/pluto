611669eeaf7a66c5d0b90ad687fbac8f700bed1c
Zac Garby <zacgarby@icloud.com>
2017-09-14 18:19:22 +0100

    Add new instructions

--- bytecode/instruction-data.go
+++ bytecode/instruction-data.go
@@ -48,6 +48,10 @@ var Instructions = map[byte]data{
 	Jump:        {Name: "JUMP", HasArg: true},
 	JumpIfTrue:  {Name: "JUMP_IF_TRUE", HasArg: true},
 	JumpIfFalse: {Name: "JUMP_IF_FALSE", HasArg: true},
+	Break:       {Name: "BREAK"},
+	Next:        {Name: "NEXT"},
+	LoopStart:   {Name: "START_LOOP"},
+	LoopEnd:     {Name: "END_LOOP"},
 
 	MakeArray: {Name: "MAKE_ARRAY", HasArg: true},
 	MakeTuple: {Name: "MAKE_TUPLE", HasArg: true},
