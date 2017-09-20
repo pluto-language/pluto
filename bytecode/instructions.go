d0e350168a2e75a568b704816573e2bbf75746f1
Zac Garby <zacgarby@icloud.com>
2017-09-20 18:57:17 +0100

    Add some comments

--- bytecode/instructions.go
+++ bytecode/instructions.go
@@ -2,25 +2,42 @@ package bytecode
 
 // 0-9: stack operations
 const (
+	// Pop pops the stack
 	Pop byte = 0
+
+	// Dup duplicates the top item, so [x, y, z] -> [x, y, z, z]
 	Dup byte = 1
+
+	// Rot rotates the top two items, so [x, y, z] -> [x, z, y]
 	Rot byte = 2
 )
 
 // 10-19: load/store
 const (
-	LoadConst  byte = 10
-	LoadName   byte = 11
-	StoreName  byte = 12
-	LoadField  byte = 13
+	// LoadConst loads a constant by index
+	LoadConst byte = 10
+
+	// LoadName loads a name by name index
+	LoadName byte = 11
+
+	// StoreName stores the top item
+	StoreName byte = 12
+
+	// LoadField pops two items, essentially does second[top]
+	LoadField byte = 13
+
+	// StoreField pops three items, essentially does second[top] = third
 	StoreField byte = 14
 )
 
 // 20-39: operators
 const (
-	UnaryInvert    byte = 20
-	UnaryNegate    byte = 21
-	UnaryNoOp      byte = 22
+	// Unary operators pop one item and do something with it
+	UnaryInvert byte = 20
+	UnaryNegate byte = 21
+	UnaryNoOp   byte = 22
+
+	// Binary operators pop two items and do something with them
 	BinaryAdd      byte = 25
 	BinarySubtract byte = 26
 	BinaryMultiply byte = 27
@@ -42,36 +59,67 @@ const (
 
 // 50-59: using functions/blocks
 const (
-	Call    byte = 50
-	Return  byte = 51
+	// Call calls a function by index,
+	// popping arguments as necessary
+	Call byte = 50
+
+	// Return skips to the end of the context
+	Return byte = 51
+
+	// DoBlock executes the block at the top of the stack,
+	// popping arguments off as necessary
 	DoBlock byte = 52
 )
 
 // 60-89: pseudo-syscalls (i.e. builtin functions?)
 const (
-	Print   byte = 60
+	// Print prints the item at the top of the stack
+	Print byte = 60
+
+	// Println prints the item at the top of the stack,
+	// with a trailing new line
 	Println byte = 61
 )
 
 // 90-99: control flow
 const (
-	Jump        byte = 90
-	JumpIfTrue  byte = 91
+	// Jump unconditionally jumps to the given offset
+	Jump byte = 90
+
+	// JumpIfTrue jumps to the given offset if the top item is truthy
+	JumpIfTrue byte = 91
+
+	// JumpIfFalse jumps to the given offset if the top item is falsey
 	JumpIfFalse byte = 92
-	Break       byte = 93
-	Next        byte = 94
-	LoopStart   byte = 95
-	LoopEnd     byte = 96
+
+	// Break jumps to the LoopEnd instruction of the innermost loop
+	Break byte = 93
+
+	// Next jumps to the LoopStart instruction of the innermost loop
+	Next byte = 94
+
+	// LoopStart pushes the start and end positions for the loop
+	LoopStart byte = 95
+
+	// LoopEnd pops the start and end positions
+	LoopEnd byte = 96
 )
 
 // 100-109: data constructors
 const (
+	// MakeArray makes an array object from the top n items
 	MakeArray byte = 100
+
+	// MakeTuple makes a tuple from the top n items
 	MakeTuple byte = 101
-	MakeMap   byte = 102
+
+	// MakeMap makes a map from the top n * 2 items.
+	// The top n*2 items should be in key, val, ..., key, val order
+	MakeMap byte = 102
 )
 
 // 110-119: packages
 const (
+	// Use directly imports the specified sources
 	Use byte = 110
 )
